6d340b1e8d5a8d15dfe118b8b01be0ae96ed4de4
rshed <rshedsfa@gmail.com>
2023-01-22 20:20:46 +0200

    any text

--- static/index.js
+++ static/index.js
@@ -23,53 +23,13 @@ function convert_Btn(){
 }
 
 
-// edit mode convert
-/* window.onload = function() {
-    document.getElementById("editeBtn").checked = true;    
-    editeMode()
-}; */
-/* 
-function editeMode(){
-    const editeMode_ON = document.getElementsByClassName("editeMode_ON");
-    const botton_w = document.getElementsByClassName("botton_w");
-    const botton_c = document.getElementsByClassName("botton_c");
-    const nameAndClass = document.getElementsByClassName("nameAndClass");
-    if(document.getElementById("editeBtn").checked){
-        document.getElementById("swichBtn").disabled = true;
 
-        for (let i = 0; i < editeMode_ON.length; i++) {
-            editeMode_ON[i].style.display="block";
-        }
-        for (let i = 0; i < botton_w.length; i++) {
-            botton_w[i].style.display="none";
-        }
-        for (let i = 0; i < botton_c.length; i++) {
-            botton_c[i].style.display="none";
-        }
-        for (let i = 0; i < nameAndClass.length; i++) {
-            nameAndClass[i].style.display="none";
-        }
+//responsive navbar
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
     }
-    else{
-        document.getElementById("swichBtn").disabled = false;
-
-        for (let i = 0; i < editeMode_ON.length; i++) {
-            editeMode_ON[i].style.display="none";
-        }
-        for (let i = 0; i < nameAndClass.length; i++) {
-            nameAndClass[i].style.display="block";
-        }
-        convert_Btn()
-    }
-}
-
-function editeModeOff(){
-    const editeMode_ON = document.getElementsByClassName("editeMode_ON");
-    document.getElementById("editeBtn").checked = false;    
-    editeMode()
-}
-
-
- */
-
-
+  }
